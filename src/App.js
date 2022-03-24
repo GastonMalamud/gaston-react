261920a9a1db5e349124f3f7d1be3c93b14a516b
Gastón <95450527+GastonMalamud@users.noreply.github.com>
2022-03-24 01:24:50 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
 import NavBar from './components/NavBar/NavBar';
 import ListProducts from './components/ListProducts/ListProducts';
+import ModalCustom from './components/Modal/Modal'
+import Card from './components/Card/Card';
+import Container from '@mui/material/Container';
 
 
 function App () {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (value) => {
+      setOpen(false);
+  };
+  
+  const handleOpen = () => {
+    setOpen(true)
+  }
 
   return (
     //JSX
     <div className="App">
     <NavBar />
-    <ListProducts/>
-
+    <Container className='container-general'> 
+        <ListProducts />
+      </Container>
     
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,49 @@
+import React,{ useState, useEffect } from 'react'
 import './Card.css'
 import Button from '@mui/material/Button';
+import ItemCount from '../ItemCount/ItemCount'
 
-export default function Card(props) {
-    console.log("Precio:", props.price)
-    console.log("Talle:", props.price)
-    console.log("Titulo:", props.price)
-    
+export default function Card({ data }) {
+    const { title, price, stock, image } = data
+    const [ count, setCount ] = useState(1)
+    const [ countTest, setCountTest ] = useState(1)
+    //const [ ]
+
+    // useEffect( () => {
+    //     console.log("SOLO MONTAJE")
+    // },[] )
+
+    // useEffect( () => {
+    //     console.log("Siempre que entre en fase ACTUALIZACION")
+    // })
+
+    // useEffect( () => {
+    //     console.log("SOLO CUANDO CAMBIE COUNT")
+    // },[count] )
+
+    // useEffect( () => {
+    //     return () => {
+    //         console.log("FASE DE DESMONTAJE")
+    //     }
+    // })
+
+    const addStock = () => {
+        setCount(count + 1)
+    }
+    const removeStock = () => {
+        setCountTest(countTest - 1)
+    }
+
     return(
         <div className="card-item">
-            {console.log("console dentro de render")}
-            <h2>{props.title}</h2>
-            <img 
-            src={props.img}
-            className='imgCard'/>
-            <p className="price">Precio : $ {props.price}</p>
-            <Button variant="contained">Comprar</Button>
+            <img src={`./${image}`} alt={image} />
+            
+            <div className='container-card-data'>
+                <h2>{title}</h2>
+                <p>Precio : $ {price}</p>
+                <ItemCount stock={stock}/>
+                
+            </div>
         </div>
     )
 }
