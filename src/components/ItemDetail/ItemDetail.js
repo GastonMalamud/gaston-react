a638eeaff9f186be0a95d8297410ef32e2ef42cf
Gastón <95450527+GastonMalamud@users.noreply.github.com>
2022-04-05 21:04:27 -0300

    Add files via upload

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,27 @@
 import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer";
+import ListProducts from '../ListProducts/ListProducts'
+import React,{ useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import ItemCount from '../ItemCount/ItemCount'
 
-function ItemDetail () {
-    
+export default function ItemDetail({ data }) {
+    const { title, price, stock, image } = data
+
+    return(
+        <div className="card-item">
+            <img src={`./${image}`} alt={image} />
+            
+            <div className='container-card-data'>
+                <h2>{title}</h2>
+                <p>Precio : $ {price}</p>
+                <p>ESPECIFICACIONES:
+                Disfruta y comparte tus mejores fotos al momento con esta cámara instantánea de Kodak. Disponible en dos acabados, esta compacta de bolsillo cuenta con un sensor de imagen y produce impresiones en papel Zink adhesivo
+                No olvides comprar papel Kodak Zink para tu Printomatic</p>
+                <ItemCount stock={stock}/>
+                <br/>
+                <Button variant="outlined" className="buttonCard">COMPRAR</Button>
+                
+            </div>
+        </div>
+    )
 }
-export default ItemDetail;
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,15 +3,16 @@ import ListProducts from "../ListProducts/ListProducts";
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
-    const mockProductos = [{
+const [dataproducts, setProducts] = useState({})
+
+    const mockProductos = {
         title : 'Kodak Printomatic',
         id: 1,
         price: 13999,
         image: '1.jpg',
         stock: 3
-    },
-]
-const [products, setProducts] = useState([])
+    }
+
 
 const getItem = () => {
     return new Promise ((resolve, reject) => {
@@ -26,6 +27,11 @@ useEffect(() => {
    })
 }, [])
 console.log(mockProductos);
+
+return(
+        <ItemDetail data={dataproducts}/>
+
+) 
 }
 
 export default ItemDetailContainer;
