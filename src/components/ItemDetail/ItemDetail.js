7dc8d4df21668ef4af9e96162365bf73ce07be28
Gastón <95450527+GastonMalamud@users.noreply.github.com>
2022-04-07 21:06:04 -0300

    Add files via upload

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -17,22 +17,19 @@ const ItemDetail = ({data}) => {
             <div className='container-detail__info'>
                 <h3 className='info__title'>{data.title}</h3>
                 <p className='info__text'>$ {data.price}</p>
-                <p className='info__subtitle'>TALLE</p>
-                <p className='info__text'>{data.talle}</p>
-                <p className='info__subtitle'>COLOR</p>
-                <ul className='info__color'>
-                    {/* {data.colors.map( (color) => {
-                        return(
-                            <li style={{background: `${color.hex}`}}></li>
-                        )
-                    })}                     */}
-                </ul>
                 <p className='info__subtitle'>DETALLE</p>
                 <p className='info__text detail__text'>{data.description}</p>
                 <Button className='detail__btn-buy'>COMPRAR</Button>
             </div>
             </div>
         </Container>
+    )
+}
+const ItemCount = () => {
+    return(
+        <div>
+            <ItemCount />
+        </div>
     )
 }
 
